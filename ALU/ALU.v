ab3aa3b9763e00b94375ec03e74e549bad3db1e9
Gustavo Inácio <gustavo.user0605@gmail.com>
2025-06-26 22:52:36 -0300

    correcoes alu

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -1,29 +1,18 @@
-module ALU(a,b,sel,out);
-	input [31:0] a;
-	input [31:0] b;
-	input [1:0] sel;
-	output reg [31:0] out;
-	output zeroFlag;
-	
-	wire reg flag
-	
-	if (out == 32'b0) begin
-		 flag = 1;
-	end else begin
-		 flag = 0;
-	end
-	
-	assign zeroFlag = flag;
-	
-	always @(a, b, sel)
-	begin
-		case(sel)
-			2'b00: out <= a+b;	
-			2'b01: out <= a-b;	
-			2'b10: out <= a&b;	
-			2'b11: out <= a|b;	
-		endcase
+module alu(a, b, sel, out, zeroFlag);
+    input [31:0] a, b;
+    input [1:0] sel;
+    output reg [31:0] out;
+    output zeroFlag;
 
-	end
- 
+    assign zeroFlag = (out == 32'b0) ? 1 : 0;
+
+    always @(*)
+    begin
+        case(sel)
+            2'b00: out <= a + b;
+            2'b01: out <= a - b;
+            2'b10: out <= a & b;
+            2'b11: out <= a | b;
+        endcase
+    end
 endmodule
