7a52ebf6f9fa9f05c967179856ab6c646bedde57
Flávio Rosim <134177338+flaviorosim@users.noreply.github.com>
2025-06-30 16:13:44 -0300

    extend certo

--- Extend/extend.v
+++ Extend/extend.v
@@ -1,18 +1,16 @@
 module extend(
 	input  [15:0] in,
-	output reg [31:0] out,
-	input en
+	output reg [31:0] out
 );
 
 	always @(*)begin
 		out <= 32'd0;
-		if(en) begin
-			if(in[15])begin
-				out <= {16'hFFFF,in[15:0]};
-			end
-			else begin
-				out <= {16'h0000,in[15:0]};
-			end
+		
+		if(in[15])begin
+			out <= {16'hFFFF,in[15:0]};
+		end
+		else begin
+			out <= {16'h0000,in[15:0]};
 		end
 	end
 	
