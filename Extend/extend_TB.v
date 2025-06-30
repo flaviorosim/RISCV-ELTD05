12730301f075d111366f86f6ad5a5be94677caf3
Flávio Rosim <134177338+flaviorosim@users.noreply.github.com>
2025-06-30 16:15:09 -0300

    esqueci do TB

--- Extend/extend_TB.v
+++ Extend/extend_TB.v
@@ -3,20 +3,18 @@
 module extend_TB ();
 
 	reg [15:0] in;
-	reg en;
+
 	wire [31:0] out;
 	
 	extend DUT (
 	.in(in),
 	.out(out),
-	.en(en)
 	);
 	
 	initial begin
-		en = 0;
+
 		in = 16'h0004;
 		#10; 
-		en = 1;
 		
 		#10; 
 		in = 16'hFFFF;
